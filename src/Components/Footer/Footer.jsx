97856f3faf3b3693660a36e6116cc91c5b013598
jubayer50 <aujubayer1@gmail.com>
2026-05-22 11:34:21 +0600

    footer content added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import { CgWebsite } from "react-icons/cg";
 import { FaFacebookF, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
+import { FiPhoneOutgoing } from "react-icons/fi";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
@@ -12,6 +15,14 @@ const Footer = () => {
                 Idea<span className="text-[#469165]">Vault</span>{" "}
               </p>
             </Link>
+
+            <div className="mt-5 max-w-55 text-white">
+              <h3 className="font-semibold text-xl">Address:</h3>
+              <p className="mt-2">
+                <strong>IdeaVault HQ </strong> 128 Innovation Avenue Suite 405
+                San Francisco, CA 94107 United States
+              </p>
+            </div>
           </div>
 
           <div>
@@ -42,6 +53,42 @@ const Footer = () => {
               >
                 My Interactions
               </Link>
+            </div>
+          </div>
+
+          <div>
+            <p className="text-white text-xl">Contacts info</p>
+
+            <div className="flex flex-col gap-2 mt-3 text-[#ffffff80]">
+              <div className="flex items-center gap-3">
+                <div className="w-10 h-10 rounded-full flex items-center justify-center bg-white">
+                  <FiPhoneOutgoing className="text-[#469165] text-xl font-medium" />
+                </div>
+                <div>
+                  <p className="font-semibold">Phone:</p>
+                  <p>+1 (555) 287-9412</p>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <div className="w-10 h-10 rounded-full flex items-center justify-center bg-white">
+                  <MdOutlineEmail className="text-[#469165] text-xl font-medium" />
+                </div>
+                <div>
+                  <p className="font-semibold">Email:</p>
+                  <p>support@ideavault.io</p>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <div className="w-10 h-10 rounded-full flex items-center justify-center bg-white">
+                  <CgWebsite className="text-[#469165] text-xl font-medium" />
+                </div>
+                <div>
+                  <p className="font-semibold">Website:</p>
+                  <p>www.ideavault.io</p>
+                </div>
+              </div>
             </div>
           </div>
 
